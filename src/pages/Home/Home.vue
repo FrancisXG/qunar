61f21dd484d975e1478bb5252b9a0afe2549de57
FrancisXG <francisliuch@outlook.com>
2021-03-24 17:31:01 +0800

    homeHeader

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <home-header></home-header>
+  <router-link to="list">222</router-link>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+ components:{
+     "home-header":Header
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home/components/Header.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont">&#xe624</span>
+            </div>
+        <div class="header-middle">
+            <input class="header-input" type="text /">
+            <span class="iconfont search-icon">&#xe632</span>
+            <div class="i-text">输入城市/景点/游玩主题</div>
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont search-icon">&#xe6aa</span></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeHeader'
+
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl'
+    
+    .header {
+        display flex
+        width 100%
+        height 0.88rem
+        background-color $bgColor
+        color #fff
+
+    }
+
+    .header .header-left {
+        width .8rem 
+        height 100%
+        line-height 0.88rem
+        text-align center
+    }
+
+    .header .header-middle {
+        position relative
+        flex 1
+        margin .14rem 0
+    }
+
+
+    .header .header-right {
+        width 1.32rem
+        height 100%
+        text-align center
+        line-height 0.88rem
+    }
+
+    .header-input{
+        width 100%
+        height 100%
+        border-radius 5px
+
+    }
+    .i-text{
+        position absolute
+        top 0.18rem 
+        left 0.5rem
+        color #e4e7ea
+    }
+
+    .header .header-middle .search-icon{
+        position absolute
+        top 0.15rem 
+        left 0.1rem
+        color #e4e7ea
+    }
+
+
+</style>
